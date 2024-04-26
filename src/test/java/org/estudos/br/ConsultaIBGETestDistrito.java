2f98ef8a430f730fa1dba0bbb812e1c457e64bd6
Allan-Perca <a.percario@hotmail.com>
2024-04-26 16:46:53 -0300

    Test status code distrito criado

--- src/test/java/org/estudos/br/ConsultaIBGETestDistrito.java
+++ src/test/java/org/estudos/br/ConsultaIBGETestDistrito.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConsultaIBGETestDistrito {
 
@@ -15,7 +19,15 @@ public class ConsultaIBGETestDistrito {
     @DisplayName("Pegando o Status Code")
     public void testConsultarDistritoStatusCode() throws IOException {
 
+        int distritoID = 320010215;
 
+        HttpURLConnection connection = (HttpURLConnection) new URL("https://servicodados.ibge.gov.br/api/v1/localidades/distritos/" + distritoID).openConnection();
+
+        connection.setRequestMethod("GET");
+
+        int statusCode = connection.getResponseCode();
+
+        assertEquals(200, statusCode, "O status code da resposta não é 200.");
 
     }
 }
