da8acf7ccce662bdcb2404154386e5e25ec15e4f
Allan-Perca <a.percario@hotmail.com>
2024-04-26 16:37:47 -0300

    Classes test criadas, Test status code estado criado, e mock test

---
+++ src/test/java/org/estudos/br/ConsultaIBGETestDistrito.java
@@ -0,0 +1,21 @@
+package org.estudos.br;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class ConsultaIBGETestDistrito {
+
+    @Test
+    @DisplayName("Pesquisando distrito")
+    public void testConsultarDistrito() throws IOException {}
+
+    @Test
+    @DisplayName("Pegando o Status Code")
+    public void testConsultarDistritoStatusCode() throws IOException {
+
+
+
+    }
+}
